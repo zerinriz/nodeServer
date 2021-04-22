7363f0c081451a5bba7212d5ec364376d01efe48
zerinriz <zerin_95@hotmail.com>
2021-04-22 20:58:15 +0200

    Prvi Projekat

---
+++ src/controller.js
@@ -0,0 +1,15 @@
+const root = (req, res) => {
+    res.end('Hello Node Server, how are you!');
+}
+
+const text = (req, res) => {
+    const param = req.params.param || 'Text';
+    res.end(`Hello ${param} Server!`);
+}
+
+const html = (req, res) => {
+    const param = req.params.param || 'Html';
+    res.end(`<h1>Hello ${param} Server!</h1>`);
+}
+
+export default { root, text, html }
